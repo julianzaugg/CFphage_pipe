8efad36d774acc0bb55f2641316ea80e2e4eb8b8
julianzaugg <julianzaugg@gmail.com>
2020-08-19 17:13:36 +1000

    racon_polish.py file checks and test print statements

--- scripts/racon_polish.py
+++ scripts/racon_polish.py
@@ -16,37 +16,43 @@ except FileExistsError:
 for round in range(snakemake.params.rounds):
     # If this is the first polishing round, generate first polished assembly
     if round == 0:
-        # Map reads
-        subprocess.Popen("minimap2 -t {} -x map-ont {} {} -o {}/{}.pol.{}.paf".format(snakemake.threads,
-                                                                        snakemake.input.assembly,
-                                                                        snakemake.input.reads,
-                                                                        out, snakemake.wildcards.sample,round),
-                         shell=True).wait()
-        # Polish
-        subprocess.Popen("racon -m 8 -x -6 -g -8 -w 500 -t {} {} {}/{}.pol.{}.paf {} > {}/{}.pol.{}.fasta".format(
-            snakemake.threads,
-            snakemake.input.reads,
-            out,snakemake.wildcards.sample,round,
-            snakemake.input.assembly,
-            out, snakemake.wildcards.sample, round
-        ), shell=True).wait()
+        if not os.path.isfile("{}/{}.pol.{}.fasta".format(out, snakemake.wildcards.sample, round)):
+            # Map reads
+            subprocess.Popen("minimap2 -t {} -x map-ont {} {} -o {}/{}.pol.{}.paf".format(snakemake.threads,
+                                                                            snakemake.input.assembly,
+                                                                            snakemake.input.reads,
+                                                                            out, snakemake.wildcards.sample,round),
+                             shell=True).wait()
+            # Polish
+            subprocess.Popen("racon -m 8 -x -6 -g -8 -w 500 -t {} {} {}/{}.pol.{}.paf {} > {}/{}.pol.{}.fasta".format(
+                snakemake.threads,
+                snakemake.input.reads,
+                out,snakemake.wildcards.sample,round,
+                snakemake.input.assembly,
+                out, snakemake.wildcards.sample, round
+            ), shell=True).wait()
     else:
         prev_round = round - 1
-        # Map reads onto the assembly from the previous round
-        subprocess.Popen("minimap2 -t {} -x map-ont {}/{}.pol.{}.fasta {} -o {}/{}.pol.{}.paf".format(snakemake.threads,
-                                                                        out,snakemake.wildcards.sample,prev_round,
-                                                                        snakemake.input.reads,
-                                                                        out,snakemake.wildcards.sample,round),
-                         shell=True).wait()
-        # Polish the assembly from the previous round
-        subprocess.Popen("racon -m 8 -x -6 -g -8 -w 500 -t {} {} {}/{}.pol.{}.paf {}/{}.pol.{}.fasta > " \
-                         "{}/{}.pol.{}.fasta".format(
-            snakemake.threads,
-            snakemake.input.reads,
-            out,snakemake.wildcards.sample,round,
-            out,snakemake.wildcards.sample,prev_round,
-            out, snakemake.wildcards.sample, round
-        ), shell=True).wait()
-    reference =  os.path.join(out, "{}/{}.pol.{}.fasta".format(out, snakemake.wildcards.sample, round))
+
+        if not os.path.isfile("{}/{}.pol.{}.fasta".format(out, snakemake.wildcards.sample, round)):
+            # Map reads onto the assembly from the previous round
+            subprocess.Popen("minimap2 -t {} -x map-ont {}/{}.pol.{}.fasta {} -o {}/{}.pol.{}.paf".format(snakemake.threads,
+                                                                            out,snakemake.wildcards.sample,prev_round,
+                                                                            snakemake.input.reads,
+                                                                            out,snakemake.wildcards.sample,round),
+                             shell=True).wait()
+            # Polish the assembly from the previous round
+            subprocess.Popen("racon -m 8 -x -6 -g -8 -w 500 -t {} {} {}/{}.pol.{}.paf {}/{}.pol.{}.fasta > " \
+                             "{}/{}.pol.{}.fasta".format(
+                snakemake.threads,
+                snakemake.input.reads,
+                out,snakemake.wildcards.sample,round,
+                out,snakemake.wildcards.sample,prev_round,
+                out, snakemake.wildcards.sample, round
+            ), shell=True).wait()
+    reference = "{}/{}.pol.{}.fasta".format(out, snakemake.wildcards.sample, round)
+print(reference)
+print(snakemake.output)
+print(repr(snakemake.output))
 shutil.copy2(reference, snakemake.output)
 
